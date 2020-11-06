2b543ec2143d89b92a30a2f55d4a0d7ff0fe23e1
lyj_gitpro <lyj_gitpro@qq.com>
2020-11-06 13:15:24 +0800

    1

--- src/main/java/testGit/test1.java
+++ src/main/java/testGit/test1.java
@@ -1,15 +1,9 @@
 package testGit;
 
 public class test1 {
-<<<<<<< HEAD
 public static void main(String[] arrgs) {
 	
 	System.out.println("git1");
-}
-=======
-	public static void main(String[] arrgs) {
-		
 		System.out.println("git");
 	}
->>>>>>> branch 'master' of https://github.com/lyj5696/testGit.git
 }
